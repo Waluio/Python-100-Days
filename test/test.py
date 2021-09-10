b550bf4b89c2c31953c00423f8e10b325e9f8b79
Waluio <glenliu007@qq.com>
2021-09-10 15:27:19 +0800

    done 02

--- test/test.py
+++ test/test.py
@@ -1,13 +1,23 @@
-"""
-第一个Python程序 - hello, world!
-向伟大的Dennis M. Ritchie先生致敬
+a = 5
+b = 10
+c = 3
+d = 4
+e = 5
+a += b
+a -= c
+a *= d
+a /= e
+print("a = ", a)
 
-Version: 0.1
-Author: 骆昊
-"""
-
-print('hello, world!')
-# print("你好,世界！")
-print('你好', '世界')
-print('hello', 'world', sep=', ', end='!\n')
-print('goodbye, world', end='!\n')
+flag1 = 3 > 2
+flag2 = 2 < 1
+flag3 = flag1 and flag2
+flag4 = flag1 or flag2
+flag5 = not flag1
+print("flag1 = ", flag1)
+print("flag2 = ", flag2)
+print("flag3 = ", flag3)
+print("flag4 = ", flag4)
+print("flag5 = ", flag5)
+print(flag1 is True)
+print(flag2 is not False)
