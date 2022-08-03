e0bb65744aeea92ddfd46d822757766fcc0d9eef
Waluio <glenliu007@qq.com>
2022-08-03 16:10:51 +0800

    stock data recieve

--- test/test.py
+++ test/test.py
@@ -1,49 +1,9 @@
-from time import sleep
-from threading import Thread
-
-
-class Account(object):
-
-    def __init__(self):
-        self._balance = 0
-
-    def deposit(self, money):
-        # 计算存款后的余额
-        new_balance = self._balance + money
-        # 模拟受理存款业务需要0.01秒的时间
-        sleep(0.01)
-        # 修改账户余额
-        self._balance = new_balance
-
-    @property
-    def balance(self):
-        return self._balance
-
-
-class AddMoneyThread(Thread):
-
-    def __init__(self, account, money):
-        super().__init__()
-        self._account = account
-        self._money = money
-
-    def run(self):
-        self._account.deposit(self._money)
-
-
-def main():
-    account = Account()
-    threads = []
-    # 创建100个存款的线程向同一个账户中存钱
-    for _ in range(100):
-        t = AddMoneyThread(account, 1)
-        threads.append(t)
-        t.start()
-    # 等所有存款的线程都执行完毕
-    for t in threads:
-        t.join()
-    print('账户余额为: ￥%d元' % account.balance)
-
-
-if __name__ == '__main__':
-    main()
+# -*- coding: utf-8 -*-
+import http.client, urllib
+conn = http.client.HTTPSConnection('api.tianapi.com')  #接口域名
+params = urllib.parse.urlencode({'key':'5005d98bbe6e42dbaff5fc0fa31d5d30','code':'hk00700'})
+headers = {'Content-type':'application/x-www-form-urlencoded'}
+conn.request('POST','/finance/index',params,headers)
+res = conn.getresponse()
+data = res.read()
+print(data.decode('utf-8'))
